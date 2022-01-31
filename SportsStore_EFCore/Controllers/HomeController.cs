2e0026e1d1ac5adf383636391a152393edb162fb
DmitriyUtyugov <utyugovkob15@gmail.com>
2022-01-31 20:33:40 +0500

    Prepared base of a project

---
+++ SportsStore_EFCore/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore_EFCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore_EFCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IRepository repository;
+
+        public HomeController(IRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public IActionResult Index() => View(repository.Products);
+
+        [HttpPost]
+        public IActionResult AddProduct(Product product)
+        {
+            repository.AddProduct(product);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ SportsStore_EFCore/Models/DataRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace SportsStore_EFCore.Models
+{
+    public class DataRepository : IRepository
+    {
+        private List<Product> data = new List<Product>();
+
+        public IEnumerable<Product> Products => data;
+
+        public void AddProduct(Product product)
+        {
+            this.data.Add(product);
+        }
+    }
+}
